aee29b59441740b5c58861c45eb794699197b49e
Daniel Stuhr Petersen <dani7354@edu.eal.dk>
2018-10-02 16:01:30 +0200

    IdentityDataContext

--- WebApplication/WebApplication/Startup.cs
+++ WebApplication/WebApplication/Startup.cs
@@ -32,6 +32,8 @@ namespace WebApplication
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<IdentityDataContext>()
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
