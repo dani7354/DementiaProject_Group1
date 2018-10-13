19eeefe0b686a64f17b4c463731f217a2c9d5b5d
Daniel Stuhr Petersen <dani7354@edu.eal.dk>
2018-10-13 14:27:25 +0200

    Log in and out

--- WebApplication/WebApplication/obj/Debug/netcoreapp2.1/Razor/Views/Home/Index.g.cshtml.cs
+++ WebApplication/WebApplication/obj/Debug/netcoreapp2.1/Razor/Views/Home/Index.g.cshtml.cs
@@ -52,7 +52,7 @@ namespace AspNetCore
             WriteLiteral("\r\n<!DOCTYPE html>\r\n\r\n<html>\r\n");
             EndContext();
             BeginContext(58, 3195, false);
-            __tagHelperExecutionContext = __tagHelperScopeManager.Begin("head", global::Microsoft.AspNetCore.Razor.TagHelpers.TagMode.StartTagAndEndTag, "c00ca0035fb746f99fd375966e6e4461", async() => {
+            __tagHelperExecutionContext = __tagHelperScopeManager.Begin("head", global::Microsoft.AspNetCore.Razor.TagHelpers.TagMode.StartTagAndEndTag, "da2b9a42929d4786b498cd34c7f77843", async() => {
                 BeginContext(64, 2801, true);
                 WriteLiteral(@"
     <meta name=""viewport"" content=""width=device-width"" />
@@ -219,7 +219,7 @@ namespace AspNetCore
             WriteLiteral("\r\n");
             EndContext();
             BeginContext(3255, 3092, false);
-            __tagHelperExecutionContext = __tagHelperScopeManager.Begin("body", global::Microsoft.AspNetCore.Razor.TagHelpers.TagMode.StartTagAndEndTag, "bb27abafa5b7403a882d6a7f1de146fd", async() => {
+            __tagHelperExecutionContext = __tagHelperScopeManager.Begin("body", global::Microsoft.AspNetCore.Razor.TagHelpers.TagMode.StartTagAndEndTag, "074c47f7e14142a380172a5a2996cfcf", async() => {
                 BeginContext(3261, 3079, true);
                 WriteLiteral(@"
     <div class=""Chat"">
