f76be079fdf6047f3e7e183558d6620a66a58cc5
Daniel Stuhr Petersen <dani7354@edu.eal.dk>
2018-10-15 11:57:17 +0200

    JS and CSS split out in files

--- WebApplication/WebApplication/wwwroot/js/chat.js
+++ WebApplication/WebApplication/wwwroot/js/chat.js
@@ -3,43 +3,41 @@ const inputForm = document.querySelector('.js-inputForm')
 const inputField = document.querySelector('.js-inputField')
 const dots = document.querySelector('.js-dots')
 
-inputForm.addEventListener('submit', (e) => {
-    const userMessage = inputField.value
-    const messageDiv = document.createElement('div')
-    const messageBubble = `<span class="Chat-bubble">${userMessage}</span>`
 
+
+function ShowUserMessage() {
+    let userMessage = inputField.value
+    let messageDiv = document.createElement('div')
+    let messageBubble = `<span class="Chat-bubble">${userMessage}</span>`
     messageDiv.classList.add('Chat-message', 'Chat-message--user')
     messageDiv.innerHTML += messageBubble;
-
     messageContainer.insertBefore(messageDiv, dots)
     messageContainer.classList.add('Chat-messages--typing')
 
-    e.preventDefault()
-
-});
-
+}
 function ShowReply(reply) {
-    const messageDiv = document.createElement('div')
+    let messageDiv = document.createElement('div')
     let messageBubble = `<span class="Chat-bubble">${reply}</span>`
     messageDiv.classList.add('Chat-message', 'Chat-message--bot')
     messageDiv.innerHTML += messageBubble;
     messageContainer.insertBefore(messageDiv, dots)
 }
-
-// Event fired when the input form is submitted (when <enter> is pressed)
-$(document).ready(function () {
-    $(".js-inputForm").submit(function () {
-        $.ajax({
-            type: "POST",
-            url: '/message/reply', 
-            data: {
-                message: $(".js-inputField").val()
-            },
-            dataType: 'json',
-            success: function (response) {
-                ShowReply(response.reply);
-            },
-        });
-        inputForm.reset();
+inputForm.addEventListener('submit', (e) => {
+    ShowUserMessage();
+    // Send user userMessage to the controller and receive response.
+    $.ajax({
+        type: "POST",
+        url: '/message/reply',
+        data: {
+            message: $(".js-inputField").val()
+        },
+        dataType: 'json',
+        success: function (response) {
+            ShowReply(response.reply);
+        },
     });
+
+    inputForm.reset();
+    e.preventDefault();
+
 });
