582f389227465fe7d18c63306f1c22fc96055fe1
Daniel Stuhr Petersen <dani7354@edu.eal.dk>
2018-10-23 16:05:04 +0200

    Reminder model + datacontext

---
+++ WebApplication/WebApplication/DataAccess/ReminderDataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication.Models;
+
+namespace WebApplication.DataAccess
+{
+    public class ReminderDataContext : DbContext
+    {
+        public DbSet<Reminder> Reminders { get; set; }
+        public ReminderDataContext(DbContextOptions<ReminderDataContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ WebApplication/WebApplication/Models/Reminder.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class Reminder
+    {
+        [Key]
+        public long Id { get; set; }
+        public string Description { get; set; }
+        public IdentityUser User { get; set; }
+        public DateTime Time { get; set; }
+        public bool ShouldRepeat { get; set; }
+
+    }
+}
