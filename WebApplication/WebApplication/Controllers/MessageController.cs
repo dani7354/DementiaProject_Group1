f0ae193ae9ba86984c1dc43d922f25002e1c0e53
Daniel Stuhr Petersen <dani7354@edu.eal.dk>
2018-10-24 12:31:39 +0200

    Google dialog flow nuGet removed

--- WebApplication/WebApplication/Controllers/MessageController.cs
+++ WebApplication/WebApplication/Controllers/MessageController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Google.Apis.Dialogflow.v2;
 using System.Net;
 using System.Net.Http;
 using System.Text;
