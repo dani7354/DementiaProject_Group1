40cb542e90e29be60b5b293129fe987f06308d5d
Daniel Stuhr Petersen <dani7354@edu.eal.dk>
2018-10-24 02:24:33 +0200

    QnA API implemented

--- WebApplication/WebApplication/Controllers/MessageController.cs
+++ WebApplication/WebApplication/Controllers/MessageController.cs
@@ -3,29 +3,79 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Google.Apis.Dialogflow.v2;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Net.Http.Headers;
+using System.Web;
+using Newtonsoft.Json;
 
 namespace WebApplication.Controllers
 {
     [Route("message")]
     public class MessageController : Controller
     {
+        public MessageController()
+        {
+           
+        }
+        public class Metadata
+        {
+            public string name { get; set; }
+            public string value { get; set; }
+        }
+
+        public class Answer
+        {
+            public IList<string> questions { get; set; }
+            public string answer { get; set; }
+            public double score { get; set; }
+            public int id { get; set; }
+            public string source { get; set; }
+            public IList<object> keywords { get; set; }
+            public IList<Metadata> metadata { get; set; }
+        }
+        public class QnAAnswer
+        {   
+            public IList<Answer> answers { get; set; }
+        }
 
         [HttpPost, Route("reply")]
-        public IActionResult GetReply(string message)
+        public async Task<IActionResult> GetReplyAsync(string message)
         {
-           if(message == null) return Json(new { Reply = $"Sorry, I didn't understand." });
-            if (message?.ToLower() == "hi")
+
+            string uri = "https://chatbotgroup1qna.azurewebsites.net/" + "/qnamaker/knowledgebases/" + "5b0943a9-3c83-4c7b-8826-408bf91b6c26" + "/generateAnswer";
+            string questionJSON = @"{'question': '" + message + "'}";
+
+            var response = await Post(uri, questionJSON);
+
+            var answers = JsonConvert.DeserializeObject<QnAAnswer>(response);
+            if (answers.answers.Count > 0)
             {
-                return Json(new { Reply = "Hello" });
-            }
-            if (message.ToLower().Contains("how are you"))
-            {
-                return Json(new { Reply = "I'm good, thanks!" });
+                Random rnd = new Random();
+                
+                return Json(new { Reply = answers.answers.ElementAt(rnd.Next(0, answers.answers.Count-1)).answer });
             }
             else
             {
-                return Json(new { Reply = $"Sorry, I didn't understand." });
+                return Json(new { Reply = "Sorry, I did not understand" }); ;
             }
         }
+        async Task<string> Post(string uri, string body)
+        {
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage())
+            {
+                request.Method = HttpMethod.Post;
+                request.RequestUri = new Uri(uri);
+                request.Content = new StringContent(body, Encoding.UTF8, "application/json");
+                request.Headers.Add("Authorization", "EndpointKey " + "d994b54e-cd11-4472-884b-088f34e59d4e");
+
+                var response = await client.SendAsync(request);
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
+
     }
 }
--- WebApplication/WebApplication/obj/Debug/netcoreapp2.1/Razor/Views/Account/Register.g.cshtml.cs
+++ WebApplication/WebApplication/obj/Debug/netcoreapp2.1/Razor/Views/Account/Register.g.cshtml.cs
@@ -49,7 +49,7 @@ namespace AspNetCore
             WriteLiteral("\r\n<!DOCTYPE html>\r\n\r\n<html>\r\n");
             EndContext();
             BeginContext(104, 103, false);
-            __tagHelperExecutionContext = __tagHelperScopeManager.Begin("head", global::Microsoft.AspNetCore.Razor.TagHelpers.TagMode.StartTagAndEndTag, "73abdd7ca62746f6ba08fbb81686d8c0", async() => {
+            __tagHelperExecutionContext = __tagHelperScopeManager.Begin("head", global::Microsoft.AspNetCore.Razor.TagHelpers.TagMode.StartTagAndEndTag, "767c9163c4fe4fea81065cc232c90bb3", async() => {
                 BeginContext(110, 90, true);
                 WriteLiteral("\r\n    <meta name=\"viewport\" content=\"width=device-width\" />\r\n    <title>Register</title>\r\n");
                 EndContext();
@@ -69,7 +69,7 @@ namespace AspNetCore
             WriteLiteral("\r\n");
             EndContext();
             BeginContext(209, 753, false);
-            __tagHelperExecutionContext = __tagHelperScopeManager.Begin("body", global::Microsoft.AspNetCore.Razor.TagHelpers.TagMode.StartTagAndEndTag, "bee10705bfc4404288f80b9cd2f3876e", async() => {
+            __tagHelperExecutionContext = __tagHelperScopeManager.Begin("body", global::Microsoft.AspNetCore.Razor.TagHelpers.TagMode.StartTagAndEndTag, "cd65845c9b9241e48e92935f113378b2", async() => {
                 BeginContext(215, 45, true);
                 WriteLiteral("\r\n    <fieldset>\r\n        <h1>Register</h1>\r\n");
                 EndContext();
